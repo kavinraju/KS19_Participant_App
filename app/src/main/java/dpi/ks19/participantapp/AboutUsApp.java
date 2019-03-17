d72198ac4c7f34f44bb6a96230e97d72aede3b9c
TANVEER <1999tanveer@gmail.com>
2019-03-17 17:14:06 +0530

    Merge branch 'master' of https://github.com/kavinraju/KS19_Participant_App
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/dpi/ks19/participantapp/MainActivity.java
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/dpi/ks19/participantapp/AboutUsApp.java
+++ app/src/main/java/dpi/ks19/participantapp/AboutUsApp.java
@@ -1,4 +1,58 @@
 package dpi.ks19.participantapp;
 
-public class AboutUsApp {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import dpi.ks19.participantapp.Adapter.GreenAdapter;
+
+public class AboutUsApp extends AppCompatActivity {
+
+    private String heading,content;
+    private static RecyclerView NameList ;
+    private GreenAdapter mAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        heading=getIntent().getStringExtra("title");
+//        content=getIntent().getStringExtra("content");
+
+        setContentView(R.layout.activity_about_us_app);
+
+//        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        ImageView ks = findViewById(R.id.imageView);
+        TextView heading = findViewById(R.id.heading);
+        TextView content = findViewById(R.id.content);
+        heading.setText(this.heading);
+
+        NameList = (RecyclerView) findViewById(R.id.content);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getBaseContext());
+        NameList.setLayoutManager(layoutManager);
+        NameList.setHasFixedSize(true);
+
+//        content.setText(this.content);
+        if(this.heading.equals("APP DEVELOPER")) {
+//            ks.setImageDrawable(getDrawable(R.drawable.cover_app_dev));
+            mAdapter = new GreenAdapter(getBaseContext(),"android");
+        }
+        else if(this.heading.equals("WEB DEVELOPER")){
+//            ks.setImageDrawable(getDrawable(R.drawable.cover_web_dev));
+            mAdapter = new GreenAdapter(getBaseContext(),"web");
+        }
+        else{
+//            ks.setImageDrawable(getDrawable(R.drawable.cover_graphic));
+            mAdapter = new GreenAdapter(getBaseContext(),"graphic");
+        }
+
+        NameList.setAdapter(mAdapter);
+    }
 }
